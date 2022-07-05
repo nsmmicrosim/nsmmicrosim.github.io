e4431142b252461730660c84c985f6299457871d
nsmmicrosim <108672545+nsmmicrosim@users.noreply.github.com>
2022-07-05 15:06:51 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,50 @@
+
+
+<!--footer -->
+    <div class="mt-4 pt-4 pb-4 footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 col-xs-12 about-company">
+
+          <h2><img src="/images/Mlogo.png" width="85" height="85" class="img-responsive" >MicroSim</h2>
+          <p class="pr-5 text-white-50">MicroSim is a project under the National Supercomputing Mission, Govt of India. The project offers a set of codes that can use high performance computing to simulate microstructure evolution using the Phase Field technique. </p>
+        </div>
+        <div class="col-lg-3 col-xs-12 links ">
+          <br><h4 class="mt-lg-0 mt-sm-3 ">Important Links</h4>
+            <ul class="m-0 p-0 footerlink ">
+
+              <li><a href="/gate"><i class="fa fa-grav" aria-hidden="true"></i> Features</a></li>
+              <li> <a  href="/psu"><i class="fa fa-university" aria-hidden="true"></i></i> Download</a></li>
+              <li> <a href="/interviews"><i class="fa fa-users" aria-hidden="true"></i> Pricing</a></li>
+              <li> <a href="/study/"><i class="fa fa-book" aria-hidden="true"></i> Our Team</a></li>
+              <li> <a href="/tests"><i class="fa fa-television" aria-hidden="true"></i> About</a></li>
+
+
+            </ul>
+        </div>
+        <div class="col-lg-4 col-xs-12 location">
+          <br><h4 class="mt-lg-0 mt-sm-4">Organising Institutes</h4>
+          <p>IISc Bangalore <br>IIT Hyderabad <br>IIT Bombay <br>IIT Madras <br>Savitribai Phule Pune University <br>C-DAC Pune</p>
+
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col copyright">
+          <p class=""><small class="text-white-50">© 2022. All Rights Reserved.</small>    </p>
+        </div>
+      </div>
+    </div>
+    </div>
+
+
+   <!-- Bootstrap core JavaScript -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+    <script src="/js/index.js"></script>
+
+   </body>
+</html>
